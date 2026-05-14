755e5e251381924c1dd9b16ff2a4bef4688a2722
Yash Kava <110620608+yashkava05@users.noreply.github.com>
2026-05-14 15:29:45 -0400

    Create queue_using_stacks.py

---
+++ Stacks and Queues/queue_using_stacks.py
@@ -0,0 +1,37 @@
+class MyQueue:
+
+    def __init__(self):
+        # making two stacks first.
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x: int) -> None:
+        # directly pushing to the first stack.
+        self.s1.append(x)
+        
+
+    def pop(self) -> int:
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2.pop()
+        
+
+    def peek(self) -> int:
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2[-1]
+        
+
+    def empty(self) -> bool:
+        return max(len(self.s1), len(self.s2)) == 0
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
